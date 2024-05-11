c1c0d55d4423cd52fcc9340479cfb93a24568a64
Riccardo Berbeglia <yawkshuh@protonmail.com>
2024-05-11 15:44:51 +0200

    Autostart pulseaudio and solaar; explain why we can't just use XDG autostart instead

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,9 +1,13 @@
 picom -CG --no-fading-openclose &
 #picom --config "$HOME/.config/picom/picom.conf" 
 
+# Since DWM does't support XDG autostart, we start things on our own
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+start-pulseaudio-x11 &
+
+pasystray &
 nitrogen --restore &
 copyq &
-pasystray &
+solaar --window=hide &
 
 exec dwm
