a2bdbc3d0b1e7dc22a508a09a94b2521c047a39f
Riccardo Berbeglia <yawkshuh@protonmail.com>
2024-05-22 18:29:16 +0200

    Replace vim with neovim if installed

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -2,14 +2,6 @@
 alias ls='ls --color=auto'
 alias ll='ls -lah'
 
-# replace cat with bat (batcat if on debian)
-if [ -f "/etc/debian_version" ]; then
-	alias cat='batcat --paging=never'
-else
-	alias cat='bat --paging=never'
-fi
-
-
 # mkdir
 alias mkdir='mkdir -p'
 
@@ -19,3 +11,15 @@ alias gl='git --no-pager log --oneline --graph --decorate --all -15'
 alias gll='git log --oneline --graph --decorate --all' # Use --no-pager here as well?
 alias gaa='git add -A'
 alias gc='git commit'
+
+# replace cat with bat (batcat if on debian)
+if [ -f "/etc/debian_version" ]; then
+	alias cat='batcat --paging=never'
+else
+	alias cat='bat --paging=never'
+fi
+
+# replace vim with neovim
+if [ -x "$(command -v nvim)" ]; then
+  alias vim='nvim'
+fi
